d09f34589914125d75ba88c52bd6e5777eafd356
Harsiddh Dave <harsiddhdave44@outlook.com>
2022-12-15 13:06:24 +0530

    Initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  'projectId': '4b7344',
-  e2e: {},
-}
+  projectId: "4b7344",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    viewportHeight: 768,
+    viewportWidth: 1366,
+    scrollBehavior: false,
+  },
+};
